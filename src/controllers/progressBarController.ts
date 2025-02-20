697ccfae2f5d175787c854ad2105b3de6aadbe57
Dodje <nostyy30@gmail.com>
2025-02-20 15:53:26 +0200

    update classroom progress done....finally

--- src/controllers/progressBarController.ts
+++ src/controllers/progressBarController.ts
@@ -8,8 +8,23 @@ export const modifiedSection = async (
   next: NextFunction,
 ) => {
   try {
-    const section = await progressBarRepo.changeSectionStatus(+req.params.id);
+    const section = await progressBarRepo.changeSectionStatus(
+      +req.params.secId,
+    );
     res.status(200).json({ message: 'Updated successfully', data: section });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatedProgress = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const updatedClassroomProgress = await progressBarRepo.updatedProgress(+req.params.classId);
+    res.status(200).json({ progress:updatedClassroomProgress.progress });
   } catch (error) {
     next(error);
   }
--- src/repos/progressBar.repo.ts
+++ src/repos/progressBar.repo.ts
@@ -2,16 +2,40 @@ import { prisma } from '../db/PrismaClient';
 import APIError from '../errors/APIError';
 
 export const progressBarRepo = {
-  async changeSectionStatus(id: number) {
+  async changeSectionStatus(secId: number) {
     const section = await prisma.section.findUnique({
-      where: { id },
+      where: { id: secId },
     });
-    if (!section) throw new APIError('No sections found', 404);
+    if (!section) throw new APIError('No section found', 404);
 
     const modifiedSection = await prisma.section.update({
-      where: { id },
+      where: { id: secId },
       data: { isCompleted: !section.isCompleted },
     });
     return modifiedSection;
+  },
+
+  async updatedProgress(classId: number) {
+    const classroom = await prisma.classroom.findUnique({
+      where: { id: classId },
+    });
+    if (!classroom) throw new APIError('No classroom found', 404);
+
+    const completedSections = await prisma.section.count({
+      where: {
+        classroomId: classId,
+        isCompleted: true,
+      },
+    });
+
+    const totalSections = await prisma.section.count({
+      where: { classroomId: classId },
+    });
+    const progress = (completedSections / totalSections) * 100;
+    const updatedClassroomProgress = await prisma.classroom.update({
+      where: { id: classId },
+      data: { progress },
+    });
+    return updatedClassroomProgress;
   },
 };
