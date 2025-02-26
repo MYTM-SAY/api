fe6cb9b65b015576f8009b6e817770b09cbab8f2
Dodje <nostyy30@gmail.com>
2025-02-26 23:14:02 +0200

    getTopTen repo

---
+++ src/repos/leaderBoard.repo.ts
@@ -0,0 +1,30 @@
+import { prisma } from '../db/PrismaClient';
+
+export const leaderBoardRepo = {
+  async getTopTen(commId: number) {
+    const result = await prisma.quizScore.findMany({
+      where: {
+        Quiz: {
+          Classroom: {
+            communityId: commId,
+          },
+        },
+      },
+      include: {
+        User: {
+          select: { 
+            id: true,
+            username: true,
+            fullname: true,
+          },
+        },
+      },
+      orderBy: {
+        score: 'desc',
+      },
+      take: 10,
+    });
+    return result;
+  },
+};
+
