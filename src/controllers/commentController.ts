cc60d56e20bdd53925f795745c8449095aabcb50
Dodje <nostyy30@gmail.com>
2025-03-01 22:49:52 +0200

    findAll comments controller

---
+++ src/controllers/commentController.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { CommentRepo } from '../repos/comment.repo';
+import APIError from '../errors/APIError';
+
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const comments = await CommentRepo.findAll();
+    if (!comments) throw new APIError('Comments not found', 404);
+    return res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
