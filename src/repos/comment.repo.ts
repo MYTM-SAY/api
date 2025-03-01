4784c112cfbf76d361051b9f5ff04476f5c5bbd8
Dodje <nostyy30@gmail.com>
2025-03-01 22:44:03 +0200

    findAll Repo

--- src/repos/comment.repo.ts
+++ src/repos/comment.repo.ts
@@ -1,38 +1,8 @@
-import { Prisma } from '@prisma/client';
 import { prisma } from '../db/PrismaClient';
 
 export const CommentRepo = {
   async findAll() {
     const results = await prisma.comment.findMany({});
     return results;
-  },
-
-  async findById(id: number) {
-    const result = await prisma.comment.findUnique({
-      where: { id },
-    });
-    return result;
-  },
-
-  async create(comment: Prisma.CommentCreateInput) {
-    const result = await prisma.comment.create({
-      data: comment,
-    });
-    return result;
-  },
-
-  async update(id: number, comment: Prisma.CommentUpdateInput) {
-    const result = await prisma.comment.update({
-      where: { id },
-      data: comment,
-    });
-    return result;
-  },
-
-  async delete(id: number) {
-    const result = await prisma.comment.delete({
-      where: { id },
-    });
-    return result;
   },
 };
