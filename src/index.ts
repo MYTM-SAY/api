50e3a36cb38ba7f13903a9e5599347cd0511ac28
Mohamed Elshafae <elshafae.official@gmail.com>
2025-03-09 23:14:05 +0200

    inject swagger => 'url'/swagger

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import error from './middlewares/error'
+import { clerkMiddleware } from '@clerk/express'
+import router from './routes'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from "swagger-ui-express";
 
-import error from './middlewares/error';
-import { clerkMiddleware } from '@clerk/express';
-import router from './routes';
-
-const app = express();
-const port = process.env.PORT || 4000;
-
-dotenv.config();
+const app = express()
+const port = process.env.PORT || 4000
+const options: swaggerJsdoc.Options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'My API',
+			version: '1.0.0',
+			description: 'API Documentation with Swagger in TypeScript',
+		},
+		servers: [
+			{
+				url: `http://localhost:${port}`,
+				description: 'Development server',
+			},
+		],
+	},
+	apis: ['./src/routes/*.ts'],
+}
+const swaggerSpecs = swaggerJsdoc(options)
+dotenv.config()
 
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    credentials: true,
-  }),
-);
-app.use(express.json());
-app.use(clerkMiddleware());
+	cors({
+		origin: process.env.CLIENT_URL || 'http://localhost:3000',
+		credentials: true,
+	}),
+)
+app.use(express.json())
+app.use(clerkMiddleware())
 app.get('/', (req, res) => {
-  return res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
+	return res.json({
+		message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+	})
+})
 
-app.use('/api/v1/', router);
-
-app.use(error);
+app.use('/api/v1/', router)
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use(error)
 
 app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+	console.log(`Listening: http://localhost:${port}`)
+})
--- src/routes/community.route.ts
+++ src/routes/community.route.ts
@@ -20,6 +20,20 @@ const app = express.Router();
 app.get('/discover', discoverCommunities);
 app.post('/remove-moderator', isAuthenticated, demoteFromModerator);
 app.post('/assign-moderator', isAuthenticated, promoteToModerator);
+/**
+ * @swagger
+ * /api/v1/communities:
+ *   get:
+ *     summary: Get all communities
+ *     description: Fetch a list of all communities.
+ *     tags: [Communities]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved communities.
+ *       500:
+ *         description: Server error.
+ */
+
 app.get('/', getCommunities);
 app.post('/', createCommunity);
 app.delete('/:id', deleteCommunity);
@@ -27,4 +41,4 @@ app.put('/:id', updateCommunity);
 app.get('/:id', getCommunity);
 app.get('/', isAuthenticated, getCommunities);
 
-export default app; 
+export default app;
