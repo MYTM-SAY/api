d484206f08f5e211168698c312ec82c066a34c42
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2025-03-17 01:40:04 +0200

    Enhance logging middleware and refactor authController methods for improved readability. Update refresh token handling to accept token from request body.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,36 +1,32 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
 import { AuthService } from '../services/authService'
 import { ResponseHelper } from '../utils/responseHelper'
 import { asyncHandler } from '../utils/asyncHandler'
 
-export const register = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { password, ...userData } = req.body
-    const newUser = await AuthService.register({ ...userData, password })
-    return res
-      .status(201)
-      .json(ResponseHelper.success('User registered successfully', newUser))
-  },
-)
+export const register = asyncHandler(async (req: Request, res: Response) => {
+  const { password, ...userData } = req.body
+  const newUser = await AuthService.register({ ...userData, password })
+  return res
+    .status(201)
+    .json(ResponseHelper.success('User registered successfully', newUser))
+})
 
-export const login = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body
-    const result = await AuthService.login(email, password)
-    return res
-      .status(200)
-      .json(ResponseHelper.success('User logged in successfully', result))
-  },
-)
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  const result = await AuthService.login(email, password)
+  return res
+    .status(200)
+    .json(ResponseHelper.success('User logged in successfully', result))
+})
 
 export const refreshToken = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const refresh = req.cookies.refreshToken
-    const result = await AuthService.refreshToken(refresh)
+  async (req: Request, res: Response) => {
+    const refreshtoken = req.cookies.refreshToken || req.body.refreshtoken
+    const result = await AuthService.refreshToken(refreshtoken)
     res.cookie('accessToken', result.accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      maxAge: 1000 * 5, // 5 seconds
+      maxAge: 1000 * 60 * 15, // 15 minutes
     })
     return res
       .status(200)
